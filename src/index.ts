8eb855cf55192799e381b9d8b6e29294a2aac03c
marllonrp <marllonrp@yahoo.com.br>
2023-01-15 15:02:38 -0300

    atividades práticas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { accounts } from './database'
+import { ACCOUNT_TYPE } from './types'
 
 const app = express()
 
@@ -17,4 +18,47 @@ app.get("/ping", (req: Request, res: Response) => {
 
 app.get("/accounts", (req: Request, res: Response) => {
     res.send(accounts)
+})
+
+
+app.get("/accounts/:id",(req: Request, res: Response) =>{
+    const id = req.params.id
+
+    const result = accounts.find(account=>account.id === id)
+    res.status(200).send(result)
+})
+
+app.delete("/accounts/:id",(req: Request, res: Response) =>{
+    const id = req.params.id
+
+    const indexToRemove = accounts.findIndex(account=>account.id === id)
+    if(indexToRemove >=0){
+        accounts.splice(indexToRemove,1)
+    }
+
+    res.status(200).send("Item deletado com sucesso.")
+})
+
+
+app.put("/accounts/:id",(req: Request, res: Response) =>{
+    const id = req.params.id
+
+    const newId = req.body.id as string |undefined
+    const neWOwnerName = req.body.ownerName as string|undefined
+    const newBalance = req.body.newBalance as number|undefined
+    const newType = req.body.type as ACCOUNT_TYPE|undefined
+
+    const accountToedit = accounts.find(account=>account.id === id)
+
+    if(accountToedit){
+        accountToedit.id = newId || accountToedit.id 
+        accountToedit.ownerName = neWOwnerName || accountToedit.ownerName
+        accountToedit.type = newType || accountToedit.type
+
+        accountToedit.balance = isNaN(newBalance)? accountToedit.balance:newBalance
+
+    }
+
+    res.status(200).send("Atualização realizada com sucesso")
+
 })
